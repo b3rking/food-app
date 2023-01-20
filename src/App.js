84cbebf20315ac51453244ce2c7362b2f84363fb
b3rking <ishimweeschylebersy@gmail.com>
2023-01-20 17:58:12 +0200

    feat: showing and hiding the cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Header from './components/Layout/Header'
-import React from 'react'
+import React, { useState } from 'react'
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+    const [showCart, setShowCart] = useState(false)
+
+    const showCartHandler = () => {
+        setShowCart(true)
+    }
+
+    const hideCartHandler = () => {
+        setShowCart(false)
+    }
+
     return (
         <React.Fragment>
-            <Header />
-            <Cart />
+            <Header onShowCart={showCartHandler}/>
+            {showCart && <Cart onHideCart={hideCartHandler}/>}
             <main>
                 <Meals />
             </main>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop}></div>;
+    return <div className={classes.backdrop} onClick={props.onHide}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -19,7 +19,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
     return (
         <React.Fragment>
-            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onHide={props.onHide}/>, portalElement)}
             {ReactDOM.createPortal(
                 <ModalOverlay>{props.children}</ModalOverlay>,
                 portalElement
